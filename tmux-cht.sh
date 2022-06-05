5c24540d9d80922fba1d87f80ee7fa520bdc7eba
Graham Albachten <albachteng@gmail.com>
2022-06-05 12:11:42 -0500

    remove sleep to avoid extra window

--- tmux-cht.sh
+++ tmux-cht.sh
@@ -10,7 +10,7 @@ read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query | less & while [ : ]; do sleep 1; done" 
+    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query | less" 
 else
     tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
 fi
