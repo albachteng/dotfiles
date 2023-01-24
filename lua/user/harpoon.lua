530de233d61c6c4f73385ee5da1a46c32ba6e15b
Graham Albachten <albachteng@gmail.com>
2023-01-24 15:19:45 -0600

    fix harpoon keymap collision

--- lua/user/harpoon.lua
+++ lua/user/harpoon.lua
@@ -4,7 +4,6 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-q>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-w>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-r>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-M>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-S>", function() ui.nav_file(3) end)
 
